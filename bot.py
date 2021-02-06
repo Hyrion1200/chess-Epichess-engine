f48c755f4e657e13a95b8694ab783b26fa54a3ad
Baptiste Nehmé <baptiste.nehme@epita.fr>
2021-02-06 02:50:12 +0000

    api working  only need game.py to fully work

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # import for .env variables
-import os, json, requests
-from dotenv import load_dotenv
+import os, json, requests, dotenv
 import game as Game
 
 APIURL = "https://lichess.org/api/"
@@ -8,8 +7,6 @@ APIURL = "https://lichess.org/api/"
 class Lichess_Bot:
     """ Instance of the bot connected to the lichess API """
 
-
-    
     def __init__(self, TOKEN):
         if TOKEN in (None, ""):
             raise Exception('Token is empty!')
@@ -18,39 +15,29 @@ class Lichess_Bot:
         self.user = self.s.get(APIURL+'account').json()
         self.ongoingGames = dict()
 
-
-
     def start(self):
-        # set the headers of the session.
-        # get the stream of event that the bot is receiving.
         self.__bot_events()
 
     def __bot_events(self):
         """ Handle all events related to the bot only """
-        response = self.s.get(APIURL+'stream/event', stream=True)
-        
-        # response = s.get(APIURL+'users/status', params={ 'ids': "gyroskan,hyrionChessBot,AleexSaan,Hyrion"})
-        # Print and check the request
-        # print("url: ", response.url)
-        # print("sent headers: ", response.request.headers)
-        # print("code:", response.status_code)
-        if response.status_code != 200:
-            print("error retrieving stream event:", response.reason)
-            print("response:", response.text)
+        rep = self.s.get(APIURL+'stream/event', stream=True)
+        if rep.status_code != 200:
+            print(f'error retrieving stream event: {rep.reason}, response: {rep.text}')
         else:
             print('Valid response')
             # iter through the events, comming in real time (infinite loop if response.close is not called)
-            for line in response.iter_lines():
+            for line in rep.iter_lines():
                 if line:
                     event = json.loads(line.decode('utf8'))
-                    print(event)
+                    # print(event)
                     if event['type'] == 'gameStart':
                         self.__game_start(event['game']['id'])
                     elif event['type'] == 'challenge':
                         self.__handle_challenge(event['challenge'])
-
+                    elif event['type'] == 'gameFinish':
+                        self.__handle_endofgame(event['game']['id'])
+        # close connection when bot stream end
         self.s.close()
-
 
     def __game_start(self, gameID):
         rep = self.s.get(APIURL+'bot/game/stream/'+gameID, stream=True)
@@ -65,24 +52,26 @@ class Lichess_Bot:
                 event = json.loads(line)
                 print(event)
                 if event['type'] == 'chatLine':
-                    if False and event['room'] == 'player' and event['username'] != self.user['username']:
+                    if False and event['room'] == 'player' and event['username'] not in (self.user['username'], 'lichess'):
                         self.__send_message(gameID, 'player', 'Hey '+event['username']+', don\'t you think you should resign?')
                 elif event['type'] == 'gameFull':
                     if event['state']['status'] == 'started':
-                        self.__create_game(event)
+                        current = self.__create_game(event)
                 elif event['type'] == 'gameState':
                     # number of moves even ==> white turn else black turn
                     if event['status'] == 'started':
                         moves = event['moves'].split(' ')
                         if self.ongoingGames[gameID].lastmove == moves[-1]:
-                            # Handle Draw events
-                            # TODO
-                            print('TODO Draw events')
+                            self.__handle_draw(gameID, event['wdraw'], event['bdraw'])
                         else:
                             current.make_move(moves[-1])
-                            botMove = current.get_move()
-                            if not self.__send_move(gameID, botMove):
-                                print('invalid move, need to redo code!')
+                            current.lastmove = moves[-1]
+                            if current.turn % 2 == current.siWhite:
+                                botMove = current.get_move()
+                                if not self.__send_move(gameID, botMove):
+                                    self.__send_resign(gameID)
+                                    self.__send_message(gameID, "player", 'I wasn\'t able to do anythin on this, well played!')
+                                    print('invalid move, *resigning*, need to redo code!')
                     elif event['status'] == 'resign':
                         if event['winner'] == self:
                             self.__send_message(gameID, 'player', 'GG EZ')
@@ -96,39 +85,10 @@ class Lichess_Bot:
         game.isWhite = event['white']['id'] == self.user['id']
         moves = event['state']['moves'].split(' ')
         for move in moves:
-            game.make_move(move)
-            game.turn += 1
+            if move != '':
+                game.make_move(move)
         game.lastmove = moves[-1]
         return game
-
-    def __send_message(self, gameID, chan, msg):
-        """ 
-        Send a message to the room 'chan' with content 'msg'
-        """
-        rep = self.s.post(APIURL+'bot/game/'+gameID+'/chat', data={ 'room': chan, 'text': msg })
-        if rep.status_code != 200:
-            print('send_messages requests not ok, code: ', rep.status_code, '\nerror: ', rep.reason, '\nreason: ', rep.text)
-        rep.close()
-
-
-    def __send_move(self, gameID, move, draw=False):
-        with self.s.post(APIURL+'bot/game/'+gameID+'/move/'+move, data={ 'offeringDraw': draw }) as rep:
-            if rep.status_code != 200 :
-                print('send_move requests not ok, code: ', rep.status_code, '\nerror: ', rep.reason, '\nreason: ', rep.text)
-                return False
-            return True
-
-    #region Challenges
-
-    def __handle_challenge(self, challenge):
-        
-        timeControl = challenge['timeControl']['type'] if challenge['timeControl']['type'] != 'clock' else challenge['timeControl']['show']
-        print('Recieved challenge. id:', challenge['id'], 'Challenger:', challenge['challenger']['id'], 'Game type:', challenge['variant']['name'], 'Rated:', challenge['rated'], 'Time', timeControl)
-        if (challenge['rated']):
-            self.__reject_challenge(challenge['id'])
-        else:
-            self.__accept_challenge(challenge['id'])
-
 
     def __accept_challenge(self, challengeID):
         """ Send an request to accept the challenge and log usefull data. """
@@ -150,34 +110,59 @@ class Lichess_Bot:
             print('Challenge rejected.\n')
         rep.close()
 
-    #endregion
+    def __handle_challenge(self, challenge):
+        """ accept or reject a challenge depending of its settings and log main infos of the challenge """
+        timeControl = challenge['timeControl']['type'] if challenge['timeControl']['type'] != 'clock' else challenge['timeControl']['show']
+        print(f'Recieved challenge. ID: {challenge["id"]}, Challenger: {challenge["challenger"]["id"]}, Game type: {challenge["variant"]["name"]}, Rated: {challenge["rated"]}, Time: {timeControl}.')
+        if (challenge['rated']):
+            self.__reject_challenge(challenge['id'])
+        else:
+            self.__accept_challenge(challenge['id'])
+
+    def __send_draw(self, gameID):
+        rep = self.s.post(f'{APIURL}bot/game/{gameID}/move/', data={ 'offeringDraw': True})
+        if rep.status_code != 200:
+            print(f'send_draw request not ok, code: {rep.status_code}, reason: {rep.text}')
+        rep.close()
+
+    def __handle_draw(self, gameID, wdraw, bdraw):
+        current = self.ongoingGames[gameID]
+        if (bdraw and current.isWhite or wdraw and not current.isWhite) and current. current.accept_draw():
+            self.__send_draw(gameID)
+
+    def __send_move(self, gameID, move, draw=False):
+        """ Send a move to make for the game with gameID, if offeringDraw, draw=True """
+        with self.s.post(APIURL+'bot/game/'+gameID+'/move/'+move, data={ 'offeringDraw': draw }) as rep:
+            if rep.status_code != 200 :
+                print(f'send_move requests not ok, code: {rep.status_code}, error: {rep.reason}, \nreason: {rep.text}')
+                return False
+            return True
+
+    def __send_message(self, gameID, chan, msg):
+        """ 
+        Send a message to the room 'chan' with content 'msg'
+        """
+        rep = self.s.post(APIURL+'bot/game/'+gameID+'/chat', data={ 'room': chan, 'text': msg })
+        if rep.status_code != 200:
+            print(f'send_messages requests not ok, code: {rep.status_code}, error: {rep.reason}, \nreason: {rep.text}')
+        rep.close()
+
+    def __send_resign(self, gameID):
+        """ Send a resign request fro the game with gameID """
+        rep = self.s.post(f'{APIURL}bot/game/{gameID}/resign')
+        if rep.status_code != 200:
+            print(f'send_resign request not ok, code: {rep.status_code}\nerror: {rep.text}')
+        rep.close()
+
+    def __handle_endofgame(self, gameID):
+        del self.ongoingGames[gameID]
+        # add anything to do after receiving end of game event
 
 if __name__=="__main__":
-    load_dotenv()
+    dotenv.load_dotenv()
     TOKEN = os.getenv("TOKEN")
     if TOKEN is None:
         print('TOKEN empty, please create .env file with TOKEN="botToken".')
         quit()
     bot = Lichess_Bot(TOKEN)
     bot.start()
-
-#region lychess api
-""" Lychess lib api
-# import for command lines arguments
-import sys, getopt
-
-# Lichess api library
-import berserk
-
-session = berserk.TokenSession(TOKEN)
-client = berserk.Client(session=session)
-
-# try:
-#     opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-# except getopt.GetoptError:
-#     print 'test.py -i <inputfile> -o <outputfile>'
-#     sys.exit(2)
-if sys.argv[1] == '-u':
-    client.account.upgrade_to_bot()
-"""
-#endregion
